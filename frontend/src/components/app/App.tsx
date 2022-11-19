beeaf219106e1ba7a4f845fab2c234e00a920d50
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-11-19 12:01:03 -0700

    move websockets to doc page

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,27 +1,11 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { Counter } from '../counter/Counter';
 import logo from '../../res/logo.svg';
-import useWebSocket from 'react-use-websocket';
-
-const API_URL = process.env['REACT_APP_API_URL'];
 
 function App() {
-  console.log(API_URL);
-  const socketUrl = 'ws://' + API_URL + '/ws';
-
-  const onMessage = (event: MessageEvent) => {
-    console.log('got msg', event);
-  };
-
-  const { sendMessage, sendJsonMessage } = useWebSocket(socketUrl, {
-    onOpen: () => console.log('opened'),
-    onMessage: onMessage,
-    // Will attempt to reconnect on all close events, such as server shutting down
-    shouldReconnect: (closeEvent) => true,
-  });
-
   return (
     <div className="App">
       <header className="App-header">
--- frontend/src/pages/DocumentPage.tsx
+++ frontend/src/pages/DocumentPage.tsx
@@ -1,3 +1,24 @@
+import { ServerResponse, ServerUpdateMessage } from '../types';
+
+import useWebSocket from 'react-use-websocket';
+
+const socketUrl = `ws://${process.env['REACT_APP_API_URL']}/ws`;
+
 export default function DocumentPage() {
+  const onMessage = (event: MessageEvent) => {
+    // Handle incoming messages here
+    // Relevant types are:
+    // ServerResponse (for responses to requests)
+    // ServerMessageUpdate (for updates to the circuit)
+    const data: ServerResponse | ServerUpdateMessage = JSON.parse(event.data);
+  };
+
+  const { sendJsonMessage } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('opened'),
+    onMessage: onMessage,
+    // Will attempt to reconnect on all close events, such as server shutting down
+    shouldReconnect: (closeEvent) => true,
+  });
+
   return <p>doc</p>;
 }
