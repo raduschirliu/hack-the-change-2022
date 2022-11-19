35f5b3af1f06cd3d1de930d49dc5abca87e8ed00
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-11-19 14:34:54 -0700

    feat: add cors

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/raduschirliu/hack-the-change-2022/database"
 	"github.com/raduschirliu/hack-the-change-2022/handlers"
@@ -25,6 +26,13 @@ func main() {
 	}
 
 	router := gin.Default()
+	cors := cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "DELETE"},
+		AllowHeaders: []string{"*"},
+	})
+
+	router.Use(cors)
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
