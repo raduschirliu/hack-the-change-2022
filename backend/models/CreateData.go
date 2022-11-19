a0c2479bcfdb886862fe63499ef0e017d951d6c7
Ratik Kapoor <ratikrk@gmail.com>
2022-11-19 13:32:40 -0700

    feat: Initial work of data models

---
+++ backend/models/CreateData.go
@@ -0,0 +1,7 @@
+package models
+
+// type CreateData struct {
+// 	ElementTypeId string `json:"elementTypeId" bson:"elementTypeId"`
+// 	X             int    `json:"x" bson:"x"`
+// 	Y             int    `json:"y" bson:"y"`
+// }
