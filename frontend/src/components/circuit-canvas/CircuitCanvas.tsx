af3cea0909e42518dc39e81e2429c92bcf9fbbae
raduschirliu <radu.schirliu@gmail.com>
2022-11-19 15:27:26 -0700

    feat: made code not uglygit add -A

--- frontend/src/components/circuit-canvas/CircuitCanvas.tsx
+++ frontend/src/components/circuit-canvas/CircuitCanvas.tsx
@@ -1,139 +1,16 @@
 import React, { useEffect, useRef } from 'react';
-import Two from 'two.js';
-import { ZUI } from 'two.js/extras/jsm/zui';
-
-// @ts-ignore
-type Group = Two.Group;
-// @ts-ignore
-type Vector = Two.Vector;
-
-const MOUSE_LEFT_BUTTON = 0;
-const MOUSE_MIDDLE_BUTTON = 1;
-
-interface ITwoRef {
-  two: Two;
-  stage: Group;
-  overlay: Group;
-  mousePos: Vector;
-  isMoving: boolean;
-  zui: ZUI;
-}
+import CircuitEditor from './CircuitEditor';
 
 export default function CircuitCanvas() {
   const twoDivRef = useRef<HTMLDivElement | null>(null);
-  let twoRef = useRef<ITwoRef | null>(null);
-
-  // Redraw the canvas
-  function draw() {
-    if (!twoRef.current) return;
-    const two = twoRef.current.two;
-
-    two.update();
-    two.render();
-
-    requestAnimationFrame(draw);
-  }
-
-  function convertMousePos(event: MouseEvent): Vector {
-    if (!twoRef.current) return;
-    const rect: DOMRect =
-      twoRef.current.two.renderer.domElement.getBoundingClientRect();
-
-    return new Two.Vector(event.clientX - rect.left, event.clientY - rect.top);
-  }
-
-  function onMouseDown(event: MouseEvent) {
-    if (!twoRef.current) return;
-    const state = twoRef.current;
-
-    const pos = convertMousePos(event);
-
-    if (event.button === MOUSE_LEFT_BUTTON) {
-      // Left click
-      const surfacePos = state.zui.clientToSurface(pos.x, pos.y);
-      const shape = new Two.Rectangle(surfacePos.x, surfacePos.y, 50, 50);
-      shape.stroke = 'red';
-      state.stage.add(shape);
-    } else if (event.button === MOUSE_MIDDLE_BUTTON) {
-      // Middle click
-      state.isMoving = true;
-    }
-  }
-
-  function onMouseUp(event: MouseEvent) {
-    if (!twoRef.current) return;
-    const state = twoRef.current;
-
-    if (event.button === MOUSE_MIDDLE_BUTTON) {
-      // Middle mouse
-      state.isMoving = false;
-    }
-  }
-
-  function onMouseMove(event: MouseEvent) {
-    if (!twoRef.current) return;
-    const state = twoRef.current;
-
-    const newPos = convertMousePos(event);
-    const deltaPos = Two.Vector.sub(newPos, state.mousePos);
-
-    if (state.isMoving) {
-      state.zui.translateSurface(deltaPos.x, deltaPos.y);
-    }
-
-    state.mousePos = newPos;
-  }
-
-  function onMouseWheel(event: WheelEvent) {}
-
-  function onKeyDown(event: KeyboardEvent) {
-    if (!twoRef.current) return;
-    const state = twoRef.current.zui;
-
-    if (event.key === 'r') {
-      state.reset();
-      state.updateSurface();
-      console.log('reset');
-    }
-  }
+  const editorRef = useRef<CircuitEditor | null>(null);
 
   // Append Two div to the dom
   useEffect(() => {
-    if (twoRef.current || !twoDivRef.current) return;
+    if (editorRef.current || !twoDivRef.current) return;
 
-    const two = new Two({ fitted: true, type: Two.Types.canvas }).appendTo(twoDivRef.current);
-    const stage = two.makeGroup();
-    const overlay = two.makeGroup();
-
-    const zui = new ZUI(stage);
-    zui.addLimits(0.06, 8);
-
-    twoRef.current = {
-      two,
-      stage,
-      overlay,
-      mousePos: new Two.Vector(0, 0),
-      isMoving: false,
-      zui,
-    };
-
-    requestAnimationFrame(draw);
-
-    console.log('initialized');
-
-    const domElement: HTMLDivElement = two.renderer.domElement;
-    domElement.addEventListener('mousedown', onMouseDown);
-    domElement.addEventListener('mouseup', onMouseUp);
-    domElement.addEventListener('mousemove', onMouseMove);
-    domElement.addEventListener('wheel', onMouseWheel);
-    window.addEventListener('keydown', onKeyDown);
-
-    // TODO: Add support for touch inputs
-    // domElement.addEventListener('touchstart', touchstart, false);
-    // domElement.addEventListener('touchmove', touchmove, false);
-    // domElement.addEventListener('touchend', touchend, false);
-    // domElement.addEventListener('touchcancel', touchend, false);
-  }, []);
+    editorRef.current = new CircuitEditor(twoDivRef.current);
+  }, [twoDivRef]);
 
   return <div className="w-full h-full" ref={twoDivRef}></div>;
 }
---
+++ frontend/src/components/circuit-canvas/CircuitEditor.ts
@@ -0,0 +1,111 @@
+import Two from 'two.js';
+import { ZUI } from 'two.js/extras/jsm/zui';
+
+// @ts-ignore
+type Group = Two.Group;
+// @ts-ignore
+type Vector = Two.Vector;
+
+const MOUSE_LEFT_BUTTON = 0;
+const MOUSE_MIDDLE_BUTTON = 1;
+
+class CircuitEditor {
+  private two: Two;
+  private stage: Group;
+  private overlay: Group;
+  private mousePos: Vector = new Two.Vector(0, 0);
+  private isMoving: boolean = false;
+  private zui: ZUI;
+
+  constructor(divRef: HTMLDivElement) {
+    this.two = new Two({ fitted: true, type: Two.Types.canvas }).appendTo(
+      divRef
+    );
+    this.stage = this.two.makeGroup();
+    this.overlay = this.two.makeGroup();
+
+    this.zui = new ZUI(this.stage);
+    this.zui.addLimits(0.06, 8);
+
+    console.log('initialized');
+
+    const domElement: HTMLDivElement = this.two.renderer.domElement;
+    domElement.addEventListener('mousedown', this.onMouseDown.bind(this));
+    domElement.addEventListener('mouseup', this.onMouseUp.bind(this));
+    domElement.addEventListener('mousemove', this.onMouseMove.bind(this));
+    domElement.addEventListener('wheel', this.onMouseWheel.bind(this));
+    window.addEventListener('keydown', this.onKeyDown.bind(this));
+
+    this.draw();
+
+    // TODO: Add support for touch inputs
+    // domElement.addEventListener('touchstart', touchstart, false);
+    // domElement.addEventListener('touchmove', touchmove, false);
+    // domElement.addEventListener('touchend', touchend, false);
+    // domElement.addEventListener('touchcancel', touchend, false);
+  }
+
+  // Redraw the canvas
+  draw() {
+    this.two.update();
+    this.two.render();
+
+    requestAnimationFrame(this.draw.bind(this));
+  }
+
+  // Reset the camera back at (0, 0)
+  resetCamera() {
+    this.zui.reset();
+    this.zui.updateSurface();
+    console.log('reset');
+  }
+
+  convertMousePos(event: MouseEvent): Vector {
+    const rect: DOMRect = this.two.renderer.domElement.getBoundingClientRect();
+
+    return new Two.Vector(event.clientX - rect.left, event.clientY - rect.top);
+  }
+
+  onMouseDown(event: MouseEvent) {
+    const pos = this.convertMousePos(event);
+
+    if (event.button === MOUSE_LEFT_BUTTON) {
+      // Left click
+      const surfacePos = this.zui.clientToSurface(pos.x, pos.y);
+      const shape = new Two.Rectangle(surfacePos.x, surfacePos.y, 50, 50);
+      shape.stroke = 'red';
+      this.stage.add(shape);
+    } else if (event.button === MOUSE_MIDDLE_BUTTON) {
+      // Middle click
+      this.isMoving = true;
+    }
+  }
+
+  onMouseUp(event: MouseEvent) {
+    if (event.button === MOUSE_MIDDLE_BUTTON) {
+      // Middle mouse
+      this.isMoving = false;
+    }
+  }
+
+  onMouseMove(event: MouseEvent) {
+    const newPos = this.convertMousePos(event);
+    const deltaPos = Two.Vector.sub(newPos, this.mousePos);
+
+    if (this.isMoving) {
+      this.zui.translateSurface(deltaPos.x, deltaPos.y);
+    }
+
+    this.mousePos = newPos;
+  }
+
+  onMouseWheel(event: WheelEvent) {}
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'r') {
+      this.resetCamera();
+    }
+  }
+}
+
+export default CircuitEditor;
