70db7dce3121e261f28ccd97b7a8c364a0daa5a9
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2022-11-19 22:35:39 -0700

    Landing page styling

---
+++ frontend/src/components/landing/LandNav.tsx
@@ -0,0 +1,61 @@
+import { ReactComponent as Hand } from '@material-design-icons/svg/filled/back_hand.svg';
+import { ReactComponent as Wire } from '@material-design-icons/svg/filled/edit.svg';
+import { ReactComponent as Delete } from '@material-design-icons/svg/filled/delete.svg';
+import { ReactComponent as Play } from '@material-design-icons/svg/filled/play_arrow.svg';
+import { isPropertySignature } from 'typescript';
+import logo from '../../res/logo.png';
+import { Link } from 'react-router-dom';
+import { clearUser } from '../../app/reducers/user';
+import { useAppDispatch } from '../../app/hooks';
+
+export function LandNav() {
+  return (
+    <nav
+      className="
+        relative
+        w-full
+        flex flex-row flex-wrap
+        items-center
+        justify-between
+        py-3
+        bg-gray-100
+        text-gray-500
+        shadow-lg
+        navbar navbar-expand-lg navbar-light
+        gap-x-4
+      "
+    >
+      <img
+        src={logo}
+        alt="our logo :)"
+        className="object-scale-down h-12 ml-4"
+        title="Home"
+      />
+      <p>App Name</p>
+      <div className="ml-auto mr-4 flex-col space-y-1 space-x-2">
+        <Link to="/sign-in">
+          <button
+            type="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            className="px-5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out"
+            title="Logout"
+          >
+            Login
+          </button>
+        </Link>
+        <Link to="/sign-up">
+          <button
+            type="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            className="px-5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out"
+            title="Logout"
+          >
+            Sign Up
+          </button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import { CircuitDocument } from '../types';
 import { Link } from 'react-router-dom';
+import useAxios from 'axios-hooks';
+import { useCallback, useState } from 'react';
+import { JoinNav } from '../components/join/JoinNav';
+import { CircuitList } from '../components/join/CircuitList';
+import { useAppDispatch } from '../app/hooks';
+import { clearUser } from '../app/reducers/user';
+import { LandNav } from '../components/landing/LandNav';
+import circuitPic from '../res/circuit.png';
 
-const Landing: React.FC = () => {
+const getDocumentsUrl = `${process.env['REACT_APP_API_URL']}/api/documents`;
+
+{
+  /* <Link to={'/sign-up'}>Sign Up</Link>
+<Link to={'/sign-in'}>Sign In</Link> */
+}
+
+export default function Landing() {
   return (
     <div>
-      <Link to={'/sign-up'}>Sign Up</Link>
-      <Link to={'/sign-in'}>Sign In</Link>
+      <LandNav />
+      {/* <!-- Jumbotron --> */}
+      <div className="h-full p-6 shadow-lg rounded-lg bg-blue-100 text-gray-700">
+        <div className="flex flex-row justify-center">
+          <img src={circuitPic} alt="circuit" className="w-1/3 p-5" />
+          <div className="flex flex-col justify-center max-w-md">
+            <h2 className="text-center font-semibold text-3xl mb-5">
+              Welcome to our multi-player digital circuit builder!<br /><br />
+              Made for teaching kids the beginnings of electrical engineering.
+            </h2>
+          </div>
+        </div>
+
+        <hr className="my-6 border-gray-300" />
+        {/* Create new circuit */}
+        <p className="text-center">A description of our app here...</p>
+        <hr className="my-6 border-gray-300" />
+        <p className="text-xs">Made with ❤️ by webbrothers</p>
+      </div>
+      {/* <!-- End jumbotron --> */}
     </div>
   );
-};
-
-export default Landing;
+}
