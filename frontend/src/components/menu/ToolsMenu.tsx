168a1b89af5c79e21b390e0806f4e9362a428e62
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2022-11-19 17:44:36 -0700

    Added home page logo/button and logout button to nav

--- frontend/src/components/menu/ToolsMenu.tsx
+++ frontend/src/components/menu/ToolsMenu.tsx
@@ -2,8 +2,15 @@ import { ReactComponent as Hand } from '@material-design-icons/svg/filled/back_h
 import { ReactComponent as Wire } from '@material-design-icons/svg/filled/edit.svg';
 import { ReactComponent as Delete } from '@material-design-icons/svg/filled/delete.svg';
 import { ReactComponent as Play } from '@material-design-icons/svg/filled/play_arrow.svg';
+import { isPropertySignature } from 'typescript';
+import logo from '../../res/logo.png';
+import { Link } from 'react-router-dom';
 
-export function ToolsMenu() {
+interface IProps {
+  documentId: string;
+}
+
+export function ToolsMenu({ documentId }: IProps) {
   return (
     <nav
       className="
@@ -22,14 +29,22 @@ export function ToolsMenu() {
         gap-x-4
       "
     >
+      <Link to="/">
+        <img
+          src={logo}
+          alt="our logo :)"
+          className="object-scale-down h-16 ml-4"
+          title="Home"
+        />
+      </Link>
       <button
         type="button"
         data-mdb-ripple="true"
         data-mdb-ripple-color="light"
-        className="ml-2 inline-block px-5 py-5 bg-gray-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        className="ml-4 inline-block px-5 py-5 bg-gray-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         title="Move"
       >
-       <Hand className="fill-white"/>
+        <Hand className="fill-white" />
       </button>
       <button
         type="button"
@@ -38,7 +53,7 @@ export function ToolsMenu() {
         className="ml-2 inline-block px-5 py-5 bg-gray-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         title="Wire Drawer"
       >
-        <Wire className="fill-white"/>
+        <Wire className="fill-white" />
       </button>
       <button
         type="button"
@@ -47,7 +62,7 @@ export function ToolsMenu() {
         className="ml-2 inline-block px-5 py-5 bg-gray-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         title="Delete"
       >
-        <Delete className="fill-white"/>
+        <Delete className="fill-white" />
       </button>
       <button
         type="button"
@@ -56,9 +71,22 @@ export function ToolsMenu() {
         className="ml-2 inline-block px-5 py-5 bg-gray-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         title="Run Simulation"
       >
-        <Play className="fill-white"/>
+        <Play className="fill-white" />
       </button>
-      <p className="ml-auto justify-end mr-2">project id: 12345</p>
+      <div className="ml-auto mr-4 flex-col space-y-1">
+        <p>Document ID: {documentId}</p>
+        <div className="flex justify-end">
+          <button
+            type="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            className="px-5 bg-gray-300 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out"
+            title="Logout"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
     </nav>
   );
 }
